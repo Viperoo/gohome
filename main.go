84a52af12437f74588a318239832082a379b9dad
Sebastian Żmijewski <sebastianzmijewski@gmail.com>
2015-01-13 15:07:55 +0100

    ETA: 1 hours 52 minutes 23 seconds (6743)

--- main.go
+++ main.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	now := time.Now()
-	deadline := time.Date(now.Year(), now.Month(), now.Day(), 17, 0, 0, 0, now.Location())
 
-	fmt.Print(now)
-	fmt.Print(deadline)
-	fmt.Print("Go home, u are drunk.")
+	for t := range time.Tick(1 * time.Second) {
+		deadline := time.Date(t.Year(), t.Month(), t.Day(), 17, 0, 0, 0, t.Location())
+		now := time.Now()
+
+		difference := deadline.Unix() - now.Unix()
+		hours := (difference / 3600)
+
+		minutes := ((difference / 60) % 60)
+		seconds := difference % 60
+
+		fmt.Printf("\x1b[31;1mETA: %[1]v hours %[2]v minutes %[3]v seconds (%[4]v)\n\x1b[0m", hours, minutes, seconds, difference)
+	}
+
 }
